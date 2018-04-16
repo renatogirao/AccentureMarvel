0270fe938a372b7c3a2dca0e057563837c7eb0f4
Renato Girão <renato.sgirao@gmail.com>
2018-04-16 00:45:54 -0300

    bug fixes and MVC implementation

---
+++ ProjetoAccentureMarvel/Models/Thumbnail.swift
@@ -0,0 +1,9 @@
+//
+//  Thumbnail.swift
+//  ProjetoAccentureMarvel
+//
+//  Created by Renato Savoia Girão on 16/04/2018.
+//  Copyright © 2018 Renato Savoia Girão. All rights reserved.
+//
+
+import Foundation
